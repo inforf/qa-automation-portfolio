42db42422ef6599f804346a5622a0ae4544909d6
Ronaldo Ferreira <inforf@hotmail.com>
2026-02-08 00:11:03 -0300

    add login test on saucedemo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('login with valid user', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page).toHaveURL(/inventory/);
 });
