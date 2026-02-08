ed4b35f14ee66ccc288b55635d18d3efe284a6e8
Ronaldo Ferreira <inforf@hotmail.com>
2026-02-08 00:18:16 -0300

    implement page object model for login

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
 
 test('login with valid user', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+  const loginPage = new LoginPage(page);
 
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
 
   await expect(page).toHaveURL(/inventory/);
 });
